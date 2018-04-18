403873bdb2820099713d9fe382b5dedd45e3b65c
magarami <m.garcia@ibermatica.com>
2018-04-18 13:51:02 +0200

    Creado el alta y consulta deLibro. El alta está funcionando mal

--- WebContent/MostrarLibros.jsp
+++ WebContent/MostrarLibros.jsp
@@ -4,26 +4,37 @@
 <!-- sentencias de import necesarias para jdbc-->
 <%@ page import="java.sql.Connection"%>
 <%@ page import="java.sql.PreparedStatement"%>
+<%@ page import="java.sql.ResultSet"%>
 <%@ page import="java.sql.DriverManager"%>
 <%@ page import="java.sql.SQLException"%>
-    
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Formulario Libro</title>
+<link rel="stylesheet" type="text/css" href="css/formato.css"></link>
+<script type="text/javascript" src="js/validacion.js"></script>
+</head>
+	<body>    
 <%
 	String DRIVER = "org.sqlite.JDBC";
 	String URL = "jdbc:sqlite:D:\\Usuarios\\Magarami\\eclipse-workspace\\ArquitecturaJava\\BBDD\\arquitecturajava.db";
 
 
-	String isbn = request.getParameter("isbn");
+/* 	String isbn = request.getParameter("isbn");
 	System.out.println("isbn: " + isbn);
 	
 	String titulo = request.getParameter("titulo");
 	System.out.println("titulo: " + titulo);
 	
 	String categoria = request.getParameter("categoria");
-	System.out.println("categoria: " + categoria);
+	System.out.println("categoria: " + categoria); */
 	
 	//Se carga el driver - Copiado de prueba conexión
 	Connection conn = null;
 	PreparedStatement pstmt = null;
+	ResultSet rs = null;
 	
 	String SQL = "Insert into LIBRO (isbn, titulo, categoria) values (?,?,?)";
 	try {
@@ -35,16 +46,18 @@
 		System.out.println("Antes de abrir la conexion");
 		//Crear la conexion
 		conn = DriverManager.getConnection(URL);
-		pstmt = conn.prepareStatement(SQL);
-		System.out.println("SQL: " + SQL);
-		
-		pstmt.setString(1, isbn);
-		pstmt.setString(2, titulo);
-		pstmt.setString(3, categoria);
-		
-		pstmt.executeUpdate();
-		System.out.println("Ejecución correcta");
-		
+		pstmt = conn.prepareStatement("Select * from LIBRO");
+		rs = pstmt.executeQuery();
+		while(rs.next()) {
+		%>	
+			<%=rs.getString("isbn") %>
+			<%=rs.getString("titulo") %>
+			<%=rs.getString("categoria") %><br></br>
+		<%
+		}
+		%>
+			<a href="./InsertarLibro.jsp">Insertar Libro</a>
+		<%
 		
 		
 	} catch (ClassNotFoundException e) {
@@ -66,40 +79,9 @@
 			e.printStackTrace();
 		}
 	}
-	
-	
-
 %>    
     
-    
-    
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Formulario Libro</title>
-<link rel="stylesheet" type="text/css" href="css/formato.css"></link>
-<script type="text/javascript" src="js/validacion.js"></script>
-</head>
-	<body>
-	<form id="miformulario" action="MostrarLibros.jsp" onsubmit="return validacion();">
-		<fieldset>
-			<legend>Formulario alta libro</legend>
-			<p>
-				<label for="isbn">ISBN:</label>
-				<input id= "isbn" type="text" name="isbn" ></input>
-			</p>
-			<p>
-				<label for="titulo">Título:</label>
-				<input id= "titulo" type="text" name="titulo" ></input>
-			</p>
-			<p>
-				<label for="titulo">Categoría:</label>
-				<input id= "categoria" type="text" name="categoria" ></input>
-			</p>
-			
-		</fieldset>
-		<input type="submit" value="Insertar"></input>
-	</form>
 	</body>
-</html>
+</html>    
+
+
--- src/mgr/PruebaConexion.java
+++ src/mgr/PruebaConexion.java
@@ -12,6 +12,8 @@ public class PruebaConexion {
 	private static final String URL = "jdbc:sqlite:D:\\Usuarios\\Magarami\\eclipse-workspace\\ArquitecturaJava\\BBDD\\arquitecturajava.db";
 	
 	public static void main(String[] args) {
+		//select();
+		//insert("2","Pepe", "Art");
 		select();
 	}
 	
@@ -32,16 +34,23 @@ public class PruebaConexion {
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
 				System.out.println("isbn: " + rs.getString("isbn"));
+				System.out.println("titulo: " + rs.getString("titulo"));
+				System.out.println("categoria: " + rs.getString("categoria"));
 			}
-			
-			
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("No se ha encontrado el driver. " + e.getMessage());
+			e.printStackTrace();
+		} catch (SQLException sqlex) {
+			// TODO: handle exception
+			System.out.println("Error al acceder a BBDD. " + sqlex.getMessage() + ". Código de error: " + sqlex.getErrorCode());
+			sqlex.printStackTrace();
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		} finally {
 			try {
-				rs.close();
 				pstmt.close();
 				conn.close();
 			} catch (SQLException e) {
@@ -53,45 +62,52 @@ public class PruebaConexion {
 	}
 	
 	//PEndiente de implementar
-//	public static void insert(String isbn, String titulo, String categoria) {
-//		Connection conn = null;
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//		
-//		String SQL = "Insert into LIBRO (isbn, titulo, categoria) values (?,?,?)";
-//		
-//		try {
-//			//Cargar el driver
-//			Class.forName(DRIVER);
-//			
-//			
-//			System.out.println("Antes de abrir la conexion");
-//			//Crear la conexion
-//			conn = DriverManager.getConnection(URL);
-//			pstmt = conn.prepareStatement(SQL);
-//			System.out.println();
-//			
-//			rs = pstmt.executeQuery();
-//			while(rs.next()) {
-//				System.out.println("isbn: " + rs.getString("isbn"));
-//			}
-//			
-//			
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//			System.out.println(e.getMessage());
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				rs.close();
-//				pstmt.close();
-//				conn.close();
-//			} catch (SQLException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//	}	
+	public static void insert(String isbn, String titulo, String categoria) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		String SQL = "Insert into LIBRO (isbn, titulo, categoria) values (?,?,?)";
+		
+		try {
+			//Cargar el driver
+			Class.forName(DRIVER);
+			
+			
+			System.out.println("Antes de abrir la conexion");
+			//Crear la conexion
+			conn = DriverManager.getConnection(URL);
+			pstmt = conn.prepareStatement(SQL);
+			System.out.println();
+			
+			pstmt.setString(1, isbn);
+			pstmt.setString(2, titulo);
+			pstmt.setString(3, categoria);
+			
+			pstmt.executeUpdate();
+
+			
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("No se ha encontrado el driver. " + e.getMessage());
+			e.printStackTrace();
+		} catch (SQLException sqlex) {
+			// TODO: handle exception
+			System.out.println("Error al acceder a BBDD. " + sqlex.getMessage() + ". Código de error: " + sqlex.getErrorCode());
+			sqlex.printStackTrace();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}	
 
 }
